ded27bad95cd82b87b4de8e8327664450afbd463
Arian <121793624+ArianNasser@users.noreply.github.com>
2023-01-05 23:37:46 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,19 @@
 # BOT SETUP
 
 BOT_TOKEN = "****"
+
 import discord
+import requests
+
 
 client = discord.Client(intents=discord.Intents.all())
 
+#bot login alert
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
+#bot says hello
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -22,12 +27,13 @@ client.run(BOT_TOKEN)
 
 # MEME GRAB USING API --- example not a meme databank, need to find a suitable API
 
-import requests
-
+# end point
 url = "https://ronreiter-meme-generator.p.rapidapi.com/meme"
 
+#parameters
 querystring = {"top":"Top Text","bottom":"Bottom Text","meme":"Condescending-Wonka","font_size":"50","font":"Impact"}
 
+#authentication
 headers = {
 	"X-RapidAPI-Key": "****",
 	"X-RapidAPI-Host": "ronreiter-meme-generator.p.rapidapi.com"

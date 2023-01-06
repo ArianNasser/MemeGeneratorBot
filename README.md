776d1c6d51c95cb2fef08bf6af4873fdc5067547
cripygummy <callum.hugh.mckenzie@gmail.com>
2023-01-06 15:01:09 +1100

    Fixing documentation + decomposition

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # MemeGeneratorBot
+
+References:
+(Add links to Discord and Meme API resources)
+
+Brief
+This program is a discord Bot that posts "Memes" gathered from external API sources before posting them in a specific discord chat.
+
+Component Description:
+
+    1. Hello
+        the Hello function replys to a !hello with "hello!"
+
+//need information on how changes can be pushed to the live bot
--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ BOT_TOKEN = "****"
 import discord
 import requests
 
-
 client = discord.Client(intents=discord.Intents.all())
 
 #bot login alert
@@ -14,13 +13,16 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
-#bot says hello
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('!hello'):
-        await message.channel.send('hello!')
+def Hello():
+    #bot says hello
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        if message.content.startswith('!hello'):
+            await message.channel.send('hello!')
+
+
 
 client.run(BOT_TOKEN)
 
